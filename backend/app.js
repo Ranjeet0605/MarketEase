548e6e5465141ad7b0f4e45ed78d569022c2a277
ranjeet0605 <kranjeet0829@gmail.com>
2024-05-23 02:47:01 +0530

    update server.js

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use("/api/v1",paymentRouter);
 
 app.use(express.static(path.join(__dirname,"../frontend/build")));
 app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+    res.sendFile(path.resolve(__dirname,"public","../frontend/build/index.html"))
 })
 // middleware for errors
 app.use(errorMiddleware);

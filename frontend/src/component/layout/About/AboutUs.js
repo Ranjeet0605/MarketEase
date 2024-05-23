ed2db4421590188bdfe30530215e9d565c6a0a89
ranjeet0605 <kranjeet0829@gmail.com>
2024-05-23 16:27:47 +0530

    search and about

--- frontend/src/component/layout/About/AboutUs.js
+++ frontend/src/component/layout/About/AboutUs.js
@@ -8,9 +8,9 @@ const AboutUs = () => {
          <h2>About Us</h2>
         <img src={profile} alt='my profile'/>
          <p className='p'>Ranjeet Kumar</p>
-         <a href='http://instagram.com/ranjeet0605'>Instagram</a>
-         <a href='http://linkdin.com/ranjeet0605'>linkdin</a>
-            <a href='http://facebook.com/ranjeet0605'>Facebook</a>
+         <a href='http://instagram.com/ranjeet_0605'>Instagram</a>
+            <a href='https://www.linkedin.com/in/ranjeet-kumar-316a17231/'>linkedin</a>
+            <a href='https://www.facebook.com/profile.php?id=100028464884635'>Facebook</a>
          <p className='p'>This is a sample webisted made by @Ranjeet kumar </p>
 
       

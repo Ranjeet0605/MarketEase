2390180e2a1137141cca61f6fdf325dcdd161fff
ranjeet0605 <kranjeet0829@gmail.com>
2024-05-23 15:13:35 +0530

    update file

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,5 +1,5 @@
 import {legacy_createStore as createStore, combineReducers,applyMiddleware,compose} from "redux";
-import thunk from "redux-thunk";
+import {thunk} from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
 
 import { ProductDetailsReducer, ProductReducer,ProductReviewReducer,deleteProductReducer,deletereviewsReducer,newProductReducer,newReviewReducer } from "./reducers/ProductReducer";
